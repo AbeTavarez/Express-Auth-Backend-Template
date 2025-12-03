55a129bbabc35024128be99d60722f815908fa33
Abraham E. Tavarez <abeincwireless@gmail.com>
2025-12-03 12:27:54 -0500

    auth updated

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,16 +6,17 @@ const expiration = "2h";
 function authMiddleware(req, res, next) {
   // Allows token to be sent via req.body, req.query, or headers
   let token = req.body?.token || req.query?.token || req.headers?.authorization;
-
+  
+  
   // We split the token string into an array and return actual token
   if (req.headers.authorization) {
     token = token.split(" ").pop().trim();
   }
-
+  
   if (!token) {
     next();
   }
-
+  
   // If token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
   try {
     const { data } = jwt.verify(token, secret, { maxAge: expiration });
